ca01cd2bc0cb18b799182cb51d35f2648f133d92
AhmedSaadawy10 <saadawyahmed42@gmail.com>
2024-03-05 14:37:25 +0200

    lab1 todo app

---
+++ todo_app/models/ticket.py
@@ -0,0 +1,13 @@
+from odoo import fields, models
+
+
+class Ticket(models.Model):
+    _name = 'todo.app.ticket'
+    _description = 'Ticket'
+
+    name = fields.Char(string="Name")
+    description = fields.Text(string="Description")
+    number = fields.Integer(string="Number")
+    tag = fields.Char(string="Tag")
+    state = fields.Selection([('new', 'New'), ('in_progress', 'In Progress'), ('done', 'Done')], string="State")
+    file = fields.Binary(string="File")
